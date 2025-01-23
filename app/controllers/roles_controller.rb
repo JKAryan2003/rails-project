3841f2df93434bd312c688ab64d632910b126e69
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-01-23 15:17:10 +0530

    initialized

---
+++ app/controllers/roles_controller.rb
@@ -0,0 +1,55 @@
+class RolesController < ApplicationController
+
+  def index
+    @roles = Role.all
+  end
+
+  def new
+    @role = Role.new
+  end
+
+  def create
+    @role = Role.new(role_params)   
+
+    if @role.save
+      redirect_to roles_path,notice: "User Created Successfully!"
+
+    else
+      flash.now[:alert] = @role.errors.full_messages.to_sentence
+      render turbo_stream: [turbo_stream.update("flash", partial: "shared/flash")]
+    end
+  end
+
+  def show
+    role
+  end
+
+  def edit
+    role
+  end
+
+  def update
+    role
+
+    if role.update(role_params)
+      redirect_to roles_path,notice: "Updated!!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    role
+    role.destroy()
+    redirect_to roles_path
+  end
+  private
+
+  def role_params
+    params.require(:role).permit(:role_name,:description)
+  end
+
+  def role
+    @role ||= Role.find_by(id: params[:id])
+  end
+end
